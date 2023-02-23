680965d240896085d1a428a1db61574bbca98070
Kristen Martin <kristen4810@gmail.com>
2023-02-23 16:21:30 +0800

    add 404.html and blog/post.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts", views.posts, name="posts")
+    path("posts", views.posts, name="posts"),
+    path("posts/<slug>", views.get_post)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, Http404
+from django.template.loader import render_to_string
 
 # Create your views here.
-
+blogs = {
+    "1": "hello this is my first post",
+    "2": "hello this is my second post",
+    "3": "hello this is my third post"
+}
 
 def index(request):
     return render(request, "blog/index.html")
@@ -10,3 +15,13 @@ def index(request):
 
 def posts(request):
     return render(request, "blog/posts.html")
+
+
+def get_post(request, slug):
+    blog_ids = list(blogs.keys())
+    if slug in blog_ids:
+        post_text = blogs[slug]
+        return render(request, "blog/post.html", { "post_text": post_text })
+    else:
+        error_page = render_to_string("404.html")
+        return HttpResponseNotFound(error_page)
