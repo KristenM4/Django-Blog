58240987a82d2b727194c7ca024ea71a1202a8a1
Kristen Martin <kristen4810@gmail.com>
2023-03-01 13:18:19 +0800

    create Author model in blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField(max_length=75)
+
+    def __str__(self):
+        return f"Full name: {self.first_name} {self.last_name}, {self.email_address}"
