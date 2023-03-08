6be657b17ea791ef95b3bba58e89b3c0698eca75
Kristen Martin <kristen4810@gmail.com>
2023-03-08 14:47:51 +0800

    create CommentAdmin, register Comment and CommentAdmin to /admin/

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author, Tag, Post
+from .models import Author, Tag, Post, Comment
 
 
 class AuthorAdmin(admin.ModelAdmin):
@@ -18,7 +18,11 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags",)
     prepopulated_fields = {"slug": ("title",)}
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "date", "comment_content", "blog_post")
+    list_filter = ("blog_post", "user_name", "date")
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
